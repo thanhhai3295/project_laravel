f1e70a587c65994961625dfbf95a816299afd8ce
pstaleal <thanhhai_1993@yahoo.com>
2020-09-17 21:21:48 +0700

    add link sidebar

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -7,7 +7,10 @@
 <div class="container body">
     <div class="main_container">
         <div class="col-md-3 left_col">
-            @include('admin.elements.sidebar')
+            <div class="left_col scroll-view">
+                @include('admin.elements.sidebar_title')
+                @include('admin.elements.sidebar_menu')
+            </div>
         </div>
         <!-- top navigation -->
         <div class="top_nav">
