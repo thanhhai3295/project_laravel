142efbce751f74a261013fb83cd1d31fe838f97b
pstaleal <thanhhai_1993@yahoo.com>
2020-10-03 15:21:45 +0700

    usercontroller

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -8,12 +8,25 @@ use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest as MainRequest;
 class UserController extends AdminController
 {
+
     public function __construct()
     {
       $this->pathViewController = 'admin.pages.user.';
       $this->controllerName     = 'user';
       parent::__construct();
       $this->model = new MainModel();
+    }
+    public function change_password(MainRequest $request){
+      $params['id'] = $request->id;
+      $params['password'] = $request->password;
+      $this->model->saveItems($params,['task' => 'change-password']);
+      return redirect()->route($this->controllerName)->with('success', 'Password Updated!');;
+    }
+    public function level(MainRequest $request){
+      $params['id'] = $request->id;
+      $params['level'] = $request->level;
+      $this->model->saveItems($params,['task' => 'change-level']);
+      return redirect()->route($this->controllerName)->with('success', 'Level Updated!');;
     }
     public function save(MainRequest $request) {
       if($request->method() == 'POST') {
